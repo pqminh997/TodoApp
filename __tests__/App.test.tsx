869b448ef15f49de20b6bccecf7c467cb23487ce
minh.pham <minh.pham@s3corp.com.vn>
2024-09-19 16:32:10 +0700

    add unit test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,122 @@
-/**
- * @format
- */
+import store, {addTodo, deleteTodo, editTodo, setTodos} from '../src/Store';
+import {TodoItem} from '../src/Utils/Types';
+import * as Util from '../src/Utils/Util';
+import * as NotiServices from '../src/Utils/NotiServices';
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+// Reset all mocks before each test
+beforeEach(() => {
+  jest.resetAllMocks();
+});
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+describe('convertText function', () => {
+  it('returns "All" for input "all"', () => {
+    expect(Util.convertText('all')).toBe('All');
+  });
+});
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+describe('addTodo action', () => {
+  it('should add a new todo to the state and call saveTodos', async () => {
+    const todo: TodoItem = {
+      id: '1',
+      title: 'Test Todo',
+      desc: 'Description for test todo',
+      expireDate: (Date.now() + 10000).toString(),
+      icon: 'none',
+      isDone: false,
+      createDate: Date.now(),
+    };
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+    // Dispatch addTodo action
+    store.dispatch(addTodo(todo));
+
+    // Check if the todo was added to the state
+    const state = store.getState();
+    expect(state.todos).toContainEqual(todo);
+
+    // Check if saveTodos was called
+    expect(Util.saveTodos).toHaveBeenCalled();
+
+    // Check if pushLocalNoti was called with the correct parameters
+    expect(NotiServices.pushLocalNoti).toHaveBeenCalledWith(
+      todo.expireDate,
+      todo.title,
+    );
+  });
+
+  it('should not call pushLocalNoti if expireDate is not provided', () => {
+    const todoWithoutExpireDate: TodoItem = {
+      id: '2',
+      title: 'No Expire Todo',
+      desc: 'This todo has no expire date',
+      expireDate: undefined,
+      icon: 'none',
+      isDone: false,
+      createDate: Date.now(),
+    };
+
+    // Dispatch addTodo action
+    store.dispatch(addTodo(todoWithoutExpireDate));
+
+    // Check if the todo was added to the state
+    const state = store.getState();
+    expect(state.todos).toContainEqual(todoWithoutExpireDate);
+
+    // Check if saveTodos was called
+    expect(Util.saveTodos).toHaveBeenCalled();
+
+    // Check that pushLocalNoti was not called if expireDate is not provided
+    expect(NotiServices.pushLocalNoti).not.toHaveBeenCalled();
+  });
+});
+
+describe('delete todo action', () => {
+  it('delete todo', () => {
+    const todoToDelete: TodoItem = {
+      id: '1',
+      title: 'Test Todo',
+      desc: 'Test Description',
+      createDate: Date.now(),
+      isDone: false,
+      icon: 'none',
+    };
+
+    store.dispatch(deleteTodo(todoToDelete));
+
+    const state = store.getState();
+    expect(state.todos).not.toContainEqual(todoToDelete);
+    expect(Util.saveTodos).toHaveBeenCalled();
+  });
+});
+
+describe('edit todo action', () => {
+  it('should edit todo without changing id and createDate', () => {
+    const existingTodo: TodoItem = {
+      id: '1',
+      title: 'Test Todo',
+      desc: 'Test Description',
+      createDate: Date.now(),
+      isDone: false,
+      icon: 'none',
+    };
+
+    const updatedTodo: TodoItem = {
+      ...existingTodo,
+      title: 'Updated Test Todo',
+      desc: 'Updated Test Description',
+      isDone: true,
+    };
+
+    store.dispatch(setTodos([existingTodo]));
+    store.dispatch(editTodo(updatedTodo));
+
+    const state = store.getState();
+
+    // Check if the todo was updated
+    expect(state.todos).toContainEqual({
+      ...updatedTodo,
+      id: existingTodo.id, // id should not change
+      createDate: existingTodo.createDate, // createDate should not change
+    });
+    expect(Util.saveTodos).toHaveBeenCalled();
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect', './__mock__/mock.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-native-calendars|react-native-swipe-gestures|@react-native|@react-native-async-storage/async-storage|@react-navigation/stack|react-native-push-notification|react-redux|uuid|@react-native-community/datetimepicker)/)',
+  ],
 };
